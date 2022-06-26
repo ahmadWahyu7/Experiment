0f5363f1a08d4282faa586aa521bda0950925a3a
ahmadWahyu7 <ahmadwahyupratama5@gmail.com>
2022-06-27 06:49:32 +0700

    Update index.js
    update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// echo "# Experiment" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin git@github.com:ahmadWahyu7/Experiment.git
+// git push -u origin main
+
